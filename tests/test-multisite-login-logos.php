302389854d0006710d52e7a3cd9ea354b6c5d0c9
zkan <kan@prontomarketing.com>
2016-02-29 06:53:45 +0700

    Initialize plugin with Dockerfile, Travis CI, and unit tests

---
+++ tests/test-multisite-login-logos.php
@@ -0,0 +1,11 @@
+<?php
+require_once( "multisite-login-logos.php" );
+
+class Multisite_Login_Logos_Test extends WP_UnitTestCase {
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function testMe() {
+    }
+}
