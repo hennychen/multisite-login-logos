a92cab26f8a16a669f033332c0bd5a4aebd22dd6
zkan <kan@prontomarketing.com>
2016-02-29 07:15:57 +0700

    Add plugin description

---
+++ multisite-login-logos.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Plugin Name: Multisite Login Logos
+ * Plugin URI: https://github.com/prontotools/multisite-logoin-logos
+ * Description: Easily change the logo on the WP login screen using WordPress's Customize settings. Choose between the default network logo, your site's logo, or any other image you upload.
+ * Author: Pronto Tools
+ * Author URI: http://www.prontotools.io
+ */
