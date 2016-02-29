7e80cd47e02132890609815e0f64ba56a24015f7
zkan <kan@prontomarketing.com>
2016-02-29 08:52:50 +0700

    Add login logo section to theme customizer

--- class-multisite-login-logos.php
+++ class-multisite-login-logos.php
@@ -5,6 +5,10 @@ class Multisite_Login_Logos {
         add_action( "customize_register", array( $this, "add_multisite_login_logos_customizer" ), 10, 1 );
     }
 
-    public function add_multisite_login_logos_customizer() {
+    public function add_multisite_login_logos_customizer( $wp_customize ) {
+        $wp_customize->add_section( "multisite_login_logos_section", array(
+            "title"    => "Login Logo",
+            "priority" => 55,
+        ) );
     }
 }
--- tests/test-multisite-login-logos.php
+++ tests/test-multisite-login-logos.php
@@ -3,8 +3,12 @@
 require_once( "multisite-login-logos.php" );
 
 class Multisite_Login_Logos_Test extends WP_UnitTestCase {
+    private $multisite_login_logos;
+
     public function setUp() {
         parent::setUp();
+
+        $this->multisite_login_logos = new Multisite_Login_Logos();
     }
 
     public function test_add_multisite_login_logos_customizer_is_registered_to_customize_register() {
@@ -21,5 +25,23 @@ class Multisite_Login_Logos_Test extends WP_UnitTestCase {
         $expected      = 1;
         $accepted_args = $wp_filter["customize_register"][10][ $key[0] ]["accepted_args"];
         $this->assertEquals( $expected, $accepted_args );
+    }
+
+    public function test_add_multisite_login_logos_customizer_should_add_login_logo_section() {
+        $wp_customize = $this->getMockBuilder( "WP_Customize_Manager" )
+            ->setMethods( array( "add_section" ) )
+            ->getMock();
+
+        $wp_customize->expects( $this->once() )
+            ->method( "add_section" )
+            ->with(
+                "multisite_login_logos_section",
+                array(
+                    "title"    => "Login Logo",
+                    "priority" => 55,
+                )
+            );
+
+        $this->multisite_login_logos->add_multisite_login_logos_customizer( $wp_customize );
     }
 }
