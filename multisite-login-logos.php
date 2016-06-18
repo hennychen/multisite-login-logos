5da08cddf1d3ad72f8493aa10250f211d1ca8ef1
zkan <kan@prontomarketing.com>
2016-06-18 11:16:51 +0700

    Add license to main file

--- multisite-login-logos.php
+++ multisite-login-logos.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Plugin Name: Multisite Login Logos
  * Plugin URI: https://github.com/prontotools/multisite-login-logos
@@ -7,6 +6,8 @@
  * Version: 1.0.2
  * Author: Pronto Tools
  * Author URI: http://www.prontotools.io
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 require_once( "class-multisite-login-logos.php" );
